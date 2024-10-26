df7ac3dbeaa146cb4f5cae2e2dbacb1818293f0c
alkihuri <alkihuri@gmail.com>
2024-10-26 12:44:21 +0200

    car movement

--- UnitySat1300.Unity/Assets/Core/Scripts/SimpleCarController.cs
+++ UnitySat1300.Unity/Assets/Core/Scripts/SimpleCarController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,13 @@ using UnityEngine;
 
 public class SimpleCarController : MonoBehaviour
 {
+    public float speed;
     public float motorForce = 1500f; // Сила, с которой машина будет двигаться
     public float rotationSpeed = 15f; // Скорость вращения машины при движении
     public float maxSpeed = 20f; // Максимальная скорость машины
+
+    public Transform forwardWheel;
+    public Transform backWheel;
 
     private Rigidbody2D rb;
 
@@ -21,6 +26,26 @@ public class SimpleCarController : MonoBehaviour
         float moveInput = Input.GetAxis("Horizontal"); // Ввод с клавиш "A/D" или "стрелки влево/вправо"
         HandleMovement(moveInput);
         HandleRotation(moveInput);
+
+        speed = rb.velocity.magnitude;
+        if (speed > 0)
+        {
+            RotateWheel();
+        }
+
+        #region Debug
+        // рисуем линию вектора скорости
+        Vector3 velocityDirection = new Vector2(transform.position.x, transform.position.y) + rb.velocity;
+        Debug.Log($"{rb.velocity} = {rb.velocity.magnitude}");
+        Debug.DrawLine(transform.position, velocityDirection, Color.red);
+        #endregion
+    }
+
+    private void RotateWheel()
+    {
+        // Вращаем переднее колесо
+        forwardWheel.eulerAngles = new Vector3(0, 0, -rb.velocity.x); 
+
     }
 
     // Движение машины вперёд и назад
