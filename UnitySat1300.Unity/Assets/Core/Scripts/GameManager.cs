710982132d5a64d328d0dce7f1050f66459d93bd
alkihuri <alkihuri@gmail.com>
2024-11-16 12:35:30 +0100

    улучшил механики управления

--- UnitySat1300.Unity/Assets/Core/Scripts/GameManager.cs
+++ UnitySat1300.Unity/Assets/Core/Scripts/GameManager.cs
@@ -6,13 +6,19 @@ public class GameManager : MonoBehaviour
 {
     public GameObject playerCar;                        // Ссылка на машину игрока
     public Vector3 startPostion;                        // Стартовая позиция игрока
+    public Vector3 startRotation;                       // Стартовое вращение игрока    
     private void Start()
     {
         startPostion = playerCar.transform.position;    // Запоминаем стартовую позицию игрока
+        startRotation = playerCar.transform.eulerAngles; // Запоминаем стартовое вращение игрока
     }
     private void OnTriggerExit2D(Collider2D collision)  // Если игрок вышел за пределы дороги
     {
-        playerCar.transform.position = startPostion;    // Возвращаем игрока на стартовую позицию
-        Debug.Log("Game Over");                         // Выводим сообщение в консоль
+        playerCar.transform.position = startPostion;                        // Возвращаем игрока на стартовую позицию
+        playerCar.transform.eulerAngles = startRotation;                    // Возвращаем игрока на стартовое вращение
+        Rigidbody2D rigidbody2D = playerCar.GetComponent<Rigidbody2D>();    // Получаем Rigidbody2D игрока
+        rigidbody2D.velocity = Vector2.zero;                                // Обнуляем скорость игрока
+        rigidbody2D.angularVelocity = 0;                                    // Обнуляем угловую скорость игрока
+        Debug.Log("Game Over");                                              // Выводим сообщение в консоль
     }
 }
